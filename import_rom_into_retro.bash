15667b3b95804d5d2a15d7a60488b2ebb9250657
zbeucler2018 <zbeucler@gmail.com>
2023-03-13 01:00:35 -0400

    dev env creation (in-container)

---
+++ import_rom_into_retro.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# By: Zack Beucler
+# Integrates the game into the gym-retro library.
+
+set -e
+
+source_path="/home/rom"
+link_name="HotWheelsStuntTrackChallenge-gba"
+lib_path="/usr/local/lib/python3.8/site-packages/retro/data/stable"
+
+if [ ! -d "$source_path" ]; then
+    echo "$source_path is not a valid directory."
+    exit 1
+fi
+
+if [ ! -d "$lib_path" ]; then
+    echo "$lib_path is not a valid directory."
+    exit 1
+fi
+
+dest_path="$lib_path/$link_name"
+
+if [ -L "$dest_path" ]; then
+    echo "Removing existing symlink: $dest_path"
+    rm "$dest_path"
+fi
+
+ln -s "$source_path" "$dest_path"
+echo "Created symlink: $dest_path -> $source_path"
+
+# import into the library
+python -m retro.import "$source_path"
