24b61f820e8ff0d2c65855eb9cf79e0a6540d71f
zackbeucler <zbeucler@gmail.com>
2023-08-20 01:36:26 -0400

    added big crop obs wrapper

--- gym_wrappers.py
+++ gym_wrappers.py
@@ -185,3 +185,18 @@ class IncreaseMeanSpeed(gym.Wrapper):
         self.mean_speed = new_mean_speed
 
         return observation, reward, terminated, truncated, info
+
+
+class NavObservation(gym.Wrapper):
+    """
+    Crops observation such that the speed dial, 
+    mini map, and lap/race timer are not included.
+    Resulting obs shape is (130, 120)
+    """
+
+    def __init__(self, env):
+        super().__init__(env)
+
+    def step(self, action):
+        observation, reward, terminated, truncated, info = self.env.step(action)
+        return observation[30:240, 55:175], reward, terminated, truncated, info
